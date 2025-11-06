929938dca01daa1d77bb54f8535d1f7c2b624524
JHON HERBERT REIN <jhon.rein@hotmail.com>
2025-11-06 16:52:22 -0300

    Create Introducao.py

---
+++ OpenCurso_Inicio/Introducao.py
@@ -0,0 +1,9 @@
+#Introdução ao Curso, primeiro contato.
+
+print("Hello Word")
+
+idade = 36
+print(idade)
+print("Testando Python") 
+
+#Ambiente visual e Sistema de controle de versao com Git e Github
