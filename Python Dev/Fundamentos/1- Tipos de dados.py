a5f900f9bc1dc73cc04e4e00fea0d5df3b21e07d
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-01-20 10:04:24 -0300

    Create 1- Tipos de dados.py

---
+++ Python Dev/Fundamentos/1- Tipos de dados.py
@@ -0,0 +1,18 @@
+#Python Flix
+
+name = "Top Gun Maverick"
+year_launch = 2023 
+noteMovie = 9.5 
+planInclueded = False 
+
+print(name)
+print(year_launch)
+
+#Usando método TYPE para saber os timpos de dados utlizados, ex: strings, float etc..
+print(type(name))
+print(type(year_launch))
+print(type(noteMovie))
+print(type(planInclueded))
+
+
+
