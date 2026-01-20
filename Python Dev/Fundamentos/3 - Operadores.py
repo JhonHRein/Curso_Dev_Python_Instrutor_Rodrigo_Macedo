21416d1ef05ec706f49df63cda9cc1f662a4d001
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-01-20 10:04:30 -0300

    Create 3 - Operadores.py

---
+++ Python Dev/Fundamentos/3 - Operadores.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o primeiro numero:\n"))
+num2 = int(input("Digite o segundo numero:\n"))
+
+#Aritiméticos 
+sum = num1 + num2
+sub =  num1 - num2
+div =  num1 / num2
+mult = num1 * num2
+
+print(f"- A soma do {num1} e {num2} é: {sum}\n- A subtração é: {sub}\n- A divisão é: {div:.2f}\n- A multiplicação é: {mult} ")
+
