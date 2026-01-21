ff736ccd4127efc2d97b162cccaa3d448693bef1
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-01-21 17:20:44 -0300

    Update 3 - Operadores.py

--- Python Dev/Fundamentos/3 - Operadores.py
+++ Python Dev/Fundamentos/3 - Operadores.py
@@ -6,6 +6,17 @@ sum = num1 + num2
 sub =  num1 - num2
 div =  num1 / num2
 mult = num1 * num2
+mod = num1 % num2  #resto da divisao
+exp = num1 ** num2  #potenciação
 
-print(f"- A soma do {num1} e {num2} é: {sum}\n- A subtração é: {sub}\n- A divisão é: {div:.2f}\n- A multiplicação é: {mult} ")
+print(f"- A soma do {num1} e {num2} é: {sum}\n- A subtração é: {sub}\n- A divisão é: {div:.2f}\n- A multiplicação é: {mult} ")  
 
+bigger = num1 > num2
+smaller = num1 < num2 
+equal = num1 == num2
+different = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2 
+
+print(bigger)
+print(smaller)
