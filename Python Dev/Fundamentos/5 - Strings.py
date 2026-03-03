3f6e642b14e1673c811b81b08f2721f59068631c
JHON HERBERT REIN <jhon.rein@hotmail.com>
2026-03-03 12:10:33 -0300

    Create 5 - Strings.py

---
+++ Python Dev/Fundamentos/5 - Strings.py
@@ -0,0 +1,20 @@
+# Python é Case sensitive:  dois valores iguais, porem um com letras maiúscula e outra minúscula existe diferença.
+
+movieName = "Top Gun Maverick"
+movieName2 = "top gun"
+print(movieName == movieName2)  # resposta FALSE, CASE SENSITIVE 
+
+
+# A utilização de """ """, abre um DocString, uma forma de texto, para uma descrição dentro de uma variavel.
+# strings multilinhas.
+movieDescription = """Top Gun Maverick é um filme de aviação e aventura\nmuito consagrado na indústria do cinemoa mundial""" 
+
+print(f"Nome do filme: {movieName}")
+ #Multiplicação de strings
+print("=-="*18) 
+print(f"Descrição:\n{movieDescription}")
+print("=-="*18)
+
+#Procurar uma palavra dentro de um texto utilizando (in).
+print("top" in movieName)
+print("ação" in movieName)  
